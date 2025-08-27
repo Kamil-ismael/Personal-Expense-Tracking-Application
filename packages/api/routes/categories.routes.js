1bdc3685a1629c61022133c0768fcede98591082
Ismael <hei.ismael.3@gmail.com>
2025-08-27 18:56:22 +0200

    add categories router file and connect getAllCategories controller

---
+++ packages/api/routes/categories.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const categoryController = require('../controllers/category.controller')
+const {autheticateToken} = require('../middleware/auth');
+
+const router = express.Router();
+router.use(autheticateToken)
+router.get('/', categoryController.getAllCategories);
