d6d847787a53b8fc2609a4019cb7935a7b544463
Ismael <hei.ismael.3@gmail.com>
2025-08-28 18:37:10 +0200

    add updateCategory controller and PUT /:id route in categories router

--- packages/api/routes/categories.routes.js
+++ packages/api/routes/categories.routes.js
@@ -5,4 +5,5 @@ const {autheticateToken} = require('../middleware/auth');
 const router = express.Router();
 router.use(autheticateToken)
 router.get('/', categoryController.getAllCategories);
-router.post('/', categoryController.createCategory);""
+router.post('/', categoryController.createCategory);
+router.put('/:id', categoryController.updateCategory);
