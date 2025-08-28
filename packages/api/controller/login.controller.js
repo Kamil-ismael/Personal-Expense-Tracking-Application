6ec9f090465f157e9a4fd4bcdf7b22210cdb2ff4
Toky <tokiniainarakotozanany@gmail.com>
2025-08-28 15:09:59 +0300

    correction des echanches req res et ajout de vérification d'email et de mot de passe et de taille de mot de passe

--- packages/api/controller/login.controller.js
+++ packages/api/controller/login.controller.js
@@ -1,9 +1,13 @@
 import jwt from "jsonwebtoken"
 import bcrypt from "bcryptjs"
-import prisma from "@prisma/client"
+import prisma from "../lib/prisma"
 
 const createUser = async (req, res)=>{
-    const {email, password} = res.body
+    const {email, password} = req.body
+
+    if ((!email || !password)||(email == "" || password == "")) {
+        res.status(400).json({message: "Email et mot de passe requis"})
+    }
 
     try{
         const checkUniqueMail = await prisma.user.findUnique(
@@ -14,12 +18,21 @@ const createUser = async (req, res)=>{
             res.status(400).json({message: "email déjà existant"})
         }
 
+        if (password.length <8) {
+            res.status(400).json({message: "Mot de passe trop faible. Le mot de passe doit contenir au moins 8 caractères."})
+        }
+
         const hashedPassword = await bcrypt.hash(password,10)
 
         const newUser = prisma.user.create({
             data:{
                 email,
                 password : hashedPassword
+            }
+        })
+        const defaultCategories = prisma.category.create({
+            data: {
+                name: ""
             }
         })
 
@@ -31,7 +44,7 @@ const createUser = async (req, res)=>{
 }
 
 const logIn = async (req, res)=>{
-        const {email, password} = res.body
+        const {email, password} = req.body
 
     try{
         const user = await prisma.user.findUnique(
