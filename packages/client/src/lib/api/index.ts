5b248cd72a4149d53fe52ab5e5b17123640cfa3f
Ismael <hei.ismael.3@gmail.com>
2025-08-31 19:19:48 +0200

    add file index.ts

---
+++ packages/client/src/lib/api/index.ts
@@ -0,0 +1,50 @@
+export * from "./types";
+export { setAuthToken, removeAuthToken, getAuthToken } from "./helpers";
+
+import { incomeApi } from "./income";
+import { categoryApi } from "./category";
+
+class Api {
+  async getCategories() {
+    return categoryApi.getCategories();
+  }
+  async createCategory(name: string) {
+    return categoryApi.createCategory(name);
+  }
+  async updateCategory(id: string, name: string) {
+    return categoryApi.updateCategory(id, name);
+  }
+  async deleteCategory(id: string) {
+    return categoryApi.deleteCategory(id);
+  }
+
+  async getIncomes(filters?: { start?: string; end?: string }) {
+    return incomeApi.getIncomes(filters);
+  }
+  async getIncome(id: string) {
+    return incomeApi.getIncome(id);
+  }
+  async createIncome(income: {
+    amount: number;
+    date: string;
+    source: string;
+    description: string;
+  }) {
+    return incomeApi.createIncome(income);
+  }
+  async updateIncome(
+    id: string,
+    income: {
+      amount: number;
+      date: string;
+      source: string;
+      description: string;
+    }
+  ) {
+    return incomeApi.updateIncome(id, income);
+  }
+  async deleteIncome(id: string) {
+    return incomeApi.deleteIncome(id);
+  }
+}
+export const api = new Api();
