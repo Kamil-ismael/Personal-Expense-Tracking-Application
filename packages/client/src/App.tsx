e3c705d6099b87c87b5b8946c89342e41d0e1843
Toky <tokiniainarakotozanany@gmail.com>
2025-09-11 14:10:32 +0300

    add protected route component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -10,18 +10,20 @@ import { AuthProvider } from "./components/AuthProviderProps";
 import { IncomeForm } from "./pages/IncomeForm";
 import { ExpenseForm } from "./pages/ExpensesForm";
 import Profile from "./pages/Profile";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+      
         <Routes>
           {/* Routes sans NavBar */}
           <Route path="/" element={<Login />} />
           <Route path="/signIn" element={<SignInPAge />} />
 
           {/* Routes avec NavBar grâce à Layout */}
-          <Route element={<Layout />}>
+          <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
             <Route path="profile" element={<Profile/>}></Route>
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/expenses" element={<Expenses />} />
